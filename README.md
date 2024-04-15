110306b2b03758eb9863e0107f4f30e2463b18d6
Anna Hilla <annahilla21@gmail.com>
2024-04-15 15:46:31 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wordle Clone
+
+A Wordle Clone created with Vite and React. This project is from the [NetNinja YouTube Channel](https://www.youtube.com/watch?v=ZSWl5UwhHcs&list=PL4cUxeGkcC9gXdVXVJBmHpSI7zCEcjLUX&index=1).
+
+<img src="./src/assets/app-preview.png" width="500" style="display:block; margin: 40px auto" />
---
+++ src/components/Keypad.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+export const Keypad = ({ usedKeys }) => {
+    const [letters, setLetters] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/letters')
+            .then(res => res.json())
+            .then(json => {
+                setLetters(json);
+            })
+    }, [])
+
+    return (
+        <div className="keypad">
+            {letters && letters.map((letter) => {
+                const color = usedKeys[letter.key];
+                return (
+                    <div className={color} key={letter.key}>{letter.key}</div>
+                )
+            })}
+        </div>
+    )
+}
